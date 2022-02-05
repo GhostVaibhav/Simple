251628a1c1d714c50907ed9e6a97efda7d7813ca
GhostVaibhav <48472541+GhostVaibhav@users.noreply.github.com>
2022-02-05 17:52:09 +0530

    Added loader initial animation

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
+import { motion } from 'framer-motion';
 
+const pageTransition = {
+    initial: {
+        x: '100vw'
+    },
+    enter: {
+        x: 0,
+    },
+}
 export class Loader extends Component {
     render() {
         return (
             <div className='overflow-hidden bg-gray-900 w-screen h-screen flex justify-end'>
-                <div className="flex items-end">
+                <motion.div transition="linear" initial="initial" animate="enter" exit="initial" variants={pageTransition} className="flex items-end">
                     <div className="loadingio-spinner-rolling-cyx4pdgtrgj"><div className="ldio-mffgx1tmlt8">
                         <div></div>
                     </div></div>
-                </div>
+                </motion.div>
             </div>
         );
     }
