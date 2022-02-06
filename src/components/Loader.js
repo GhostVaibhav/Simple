1a1401f425bcabbb92096dd82a2c8a1a970681fd
GhostVaibhav <48472541+GhostVaibhav@users.noreply.github.com>
2022-02-06 16:17:40 +0530

    Changed loader location and animation

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,23 +1,13 @@
 import React, { Component } from 'react';
-import { motion } from 'framer-motion';
-
-const pageTransition = {
-    initial: {
-        x: '100vw'
-    },
-    enter: {
-        x: 0,
-    },
-}
 export class Loader extends Component {
     render() {
         return (
-            <div className='overflow-hidden bg-gray-900 w-screen h-screen flex justify-end'>
-                <motion.div transition="linear" initial="initial" animate="enter" exit="initial" variants={pageTransition} className="flex items-end">
+            <div className='overflow-hidden bg-gray-900 w-screen h-screen flex justify-center'>
+                <div className="flex items-center">
                     <div className="loadingio-spinner-rolling-cyx4pdgtrgj"><div className="ldio-mffgx1tmlt8">
                         <div></div>
                     </div></div>
-                </motion.div>
+                </div>
             </div>
         );
     }
