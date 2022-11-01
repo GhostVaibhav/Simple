f66768db52cef3440542eb640f4eba6f403de29c
GhostVaibhav <48472541+GhostVaibhav@users.noreply.github.com>
2022-11-01 14:28:17 +0530

    Created Theme changer logic (WIP)

---
+++ src/components/ThemeButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ThemeButton(props) {
+    const theming = {
+        "bg-[#059669]": "green",
+        "bg-[#ff220e]": "red",
+        "bg-[#2563eb]": "blue"
+    }
+    return (
+        <button className={`${props.active ? "border-white" : ""} h-4 w-10 mx-2 my-4 border-2 border-transparent ${props.bgColor} rounded-full`} onClick={() => props.setTheme(theming[props.bgColor])}></button>
+    )
+}
